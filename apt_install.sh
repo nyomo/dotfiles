fa96a97bc74b19e83950023adc0bff30a883593d
nyomo <nyomoko@gmail.com>
2018-12-15 21:51:23 +0900

    ruby-buildに必要なものとデバッガ

--- apt_install.sh
+++ apt_install.sh
@@ -2,6 +2,7 @@
 LIST="make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
 xz-utils tk-dev libffi-dev liblzma-dev \
-binutils-mingw-w64
+binutils-mingw-w64 gdb \
+autoconf bison libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm5 libgdbm-dev \
 "
 sudo apt install -y $LIST
