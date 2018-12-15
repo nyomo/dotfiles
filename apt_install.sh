aef943856ef8a0305db3977be129a7024d73a8c6
nyomo <nyomoko@gmail.com>
2018-12-15 21:19:08 +0900

    たまにbinutils必要になる

--- apt_install.sh
+++ apt_install.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 LIST="make build-essential libssl-dev zlib1g-dev libbz2-dev \
 libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev \
-xz-utils tk-dev libffi-dev liblzma-dev"
+xz-utils tk-dev libffi-dev liblzma-dev \
+binutils-mingw-w64
+"
 sudo apt install -y $LIST
