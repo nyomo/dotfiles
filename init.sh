4114e9447ad789d5765a804b1b2d7a689e2f1fd7
nyomo <nyomoko@gmail.com>
2018-12-15 20:43:44 +0900

    リンクを作るスクリプト

---
+++ init.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo locale-gen ja_JP.UTF-8
+DOTFILEDIR=~/.dotfiles
+
+ln -s $DOTFILEDIR/.bash_profile
+ln -s $DOTFILEDIR/.git
+ln -s $DOTFILEDIR/.gitconfig
+ln -s $DOTFILEDIR/.tmux.conf
+ln -s $DOTFILEDIR/.vimrc
